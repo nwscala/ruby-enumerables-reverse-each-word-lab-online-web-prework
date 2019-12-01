1b1a01616b9b18eab4c0d1499dc4a22d486c1492
Noah Scala <tobi8912@gmail.com>
2019-12-01 18:54:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word_with_each(string)
   original_array = string.split("")
   reverse_array = []
   original_array.each do |string|
-    
+    reverse_array.push(string.reverse)
   end
 end 

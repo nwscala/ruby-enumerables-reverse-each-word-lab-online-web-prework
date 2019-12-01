641a16d0db1272410861000fb3fd7759932a3d4e
Noah Scala <tobi8912@gmail.com>
2019-12-01 18:55:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word_with_each(string)
   end
   reverse_array.join(" ")
 end 
+
+def reverse_each_word(string)
+  original_array = string.split(" ")
+  reverse_array = []
+  original_array.each do |string|
+    reverse_array.push(string.reverse)
+  end
+  reverse_array.join(" ")
+end 
